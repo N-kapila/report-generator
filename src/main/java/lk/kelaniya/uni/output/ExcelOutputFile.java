1ef70eaf902abf9d9a576559e094f6264e58c9e3
NethminiDevyanjalee <73274982+NethminiDevyanjalee@users.noreply.github.com>
2021-10-02 17:36:39 +0530

    updated implement output

--- src/main/java/lk/kelaniya/uni/output/ExcelOutputFile.java
+++ src/main/java/lk/kelaniya/uni/output/ExcelOutputFile.java
@@ -1,72 +1,64 @@
 package lk.kelaniya.uni.output;
 
+import lk.kelaniya.uni.repository.DataResult;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.ArrayList;
+
 
 public class ExcelOutputFile implements OutputFile {
 
+    final private DataResult dataResult;
+    final private String fileName;
+
+
+    public ExcelOutputFile(DataResult dataResult, String fileName) {
+        this.dataResult = dataResult;
+        this.fileName = fileName;
+    }
+
     @Override
     public void write() throws OutputException {
-        //Create blank workbook instance
-        XSSFWorkbook workbook = new XSSFWorkbook();
+        final ArrayList<String> fieldsNames = dataResult.getFieldNames();
+        final ArrayList<ArrayList<Object>> records = dataResult.getRecords();
 
-        //Create a blank sheet inside the workbook
-        XSSFSheet sheet = workbook.createSheet("Sheet1");
+        final XSSFWorkbook workbook = new XSSFWorkbook();
+        final XSSFSheet sheet = workbook.createSheet("Sheet1");
 
-        // Setting Foreground Color
-        CellStyle style = workbook.createCellStyle();
-        style.setFillForegroundColor(IndexedColors.PLUM.getIndex());
-        style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        Row row = null;
+        Cell cell = null;
 
-        //This data needs to be written (Object[])
-        Map<String, Object[]> data = new TreeMap<String, Object[]>();
-        data.put("1", new Object[]{"ID", "NAME", "LASTNAME"});
-        data.put("2", new Object[]{1, "Amit", "Shukla"});
-        data.put("3", new Object[]{2, "Lokesh", "Gupta"});
-        data.put("4", new Object[]{3, "John", "Adwards"});
-        data.put("5", new Object[]{4, "Brian", "Schultz"});
+        row = sheet.createRow(0);
 
-        //Iterate over data and write to sheet
-        Set<String> keyset = data.keySet();
-        int rownum = 0;
-        for (String key : keyset) {
-            // create a new row
-            Row row = sheet.createRow(rownum++);
-            Object[] objArr = data.get(key);
-            int cellnum = 0;
-            for (Object obj : objArr) {
-                // create a new cell
-                Cell cell = row.createCell(cellnum++);
+        for (int i = 0; i < fieldsNames.size(); i++) {
+            cell = row.createCell(i);
+            cell.setCellValue(fieldsNames.get(i));
+        }
 
-                if (rownum==1){
-                    cell.setCellStyle(style);
-                }
-
-                // set the value in the cell
-                if (obj instanceof String)
-                    cell.setCellValue((String) obj);
-                else if (obj instanceof Integer)
-                    cell.setCellValue((Integer) obj);
+        for (int i = 0; i < records.size(); i++) {
+            row = sheet.createRow(i + 1);
+            for (int j = 0; j < fieldsNames.size(); j++) {
+                cell = row.createCell(j);
+                Object obj = records.get(i).get(j);
+                cell.setCellValue(obj.toString());
             }
         }
 
         try {
             //Write the workbook in file system
-            FileOutputStream out = new FileOutputStream(new File("C:\\Users\\NETHMINIDEVYANJALEE\\Desktop\\demo1.xlsx"));
+            FileOutputStream out = new FileOutputStream(new File("" + fileName + ".xlsx"));
             workbook.write(out);
             out.close();
-            System.out.println("demo1.xlsx written successfully.");
         } catch (Exception e) {
-             throw new OutputException(e,"");
+            throw new OutputException(e, e.getMessage());
         }
+
 
     }
 }
+
 
