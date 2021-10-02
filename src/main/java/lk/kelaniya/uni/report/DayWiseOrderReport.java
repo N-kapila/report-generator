aecad6bd1242dfe77bff23443bc8a76df158ea9b
Imasha Weerakoon <67216030+imashaWe@users.noreply.github.com>
2021-10-03 01:18:48 +0530

    integrate_day_wise_sale_report (#7)
    * implemented data report
    
    * implemented DayWiseOrderReport

---
+++ src/main/java/lk/kelaniya/uni/report/DayWiseOrderReport.java
@@ -0,0 +1,32 @@
+package lk.kelaniya.uni.report;
+
+import lk.kelaniya.uni.repository.DataRepository;
+import lk.kelaniya.uni.repository.DataRepositoryException;
+import lk.kelaniya.uni.repository.DataResult;
+
+public class DayWiseOrderReport implements DataReport {
+    final private DataRepository dataRepository;
+
+    final private String startDate;
+    final private String endDate;
+
+    public DayWiseOrderReport(DataRepository dataRepository, String startDate, String endDate) {
+        this.dataRepository = dataRepository;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    @Override
+    public DataResult generate() throws DataRepositoryException {
+        String query = "SELECT" +
+                "`orderDate`," +
+                "COUNT(DISTINCT `orders`.orderNumber) AS 'Number of Orders'," +
+                "SUM(`quantityOrdered`*`priceEach`) AS 'Income'" +
+                "FROM `orderdetails`" +
+                "INNER JOIN `orders` ON `orders`.`orderNumber`= `orderdetails`.`orderNumber`" +
+                "WHERE `orderDate` BETWEEN '" + startDate + "' AND '" + endDate + "'"+
+                "GROUP BY DAY(`orderDate`)";
+
+        return dataRepository.executeQuery(query);
+    }
+}
