3974c1c562f43b1fb0910cad10203acaa86fb86d
savitarasitha <asithanuwanbandara@gmail.com>
2021-10-02 15:59:24 +0530

    UI module successfully created

---
+++ src/main/java/lk/kelaniya/uni/UI/CmdLineUI.java
@@ -0,0 +1,11 @@
+package lk.kelaniya.uni.UI;
+
+public class CmdLineUI implements UI {
+    public void showSuccessMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void showErrorMessage(String message) {
+        System.out.println(message);
+    }
+}
