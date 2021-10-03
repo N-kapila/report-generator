0e04ff2d09c7bea07427e9b5a9f0ccc436da2199
NethminiDevyanjalee <73274982+NethminiDevyanjalee@users.noreply.github.com>
2021-10-03 17:39:15 +0530

    unit test for DataReportFactory

--- src/test/java/lk/kelaniya/uni/report/DataReportFactoryTest.java
+++ src/test/java/lk/kelaniya/uni/report/DataReportFactoryTest.java
@@ -1,7 +1,27 @@
 package lk.kelaniya.uni.report;
 
-import static org.junit.Assert.*;
+import lk.kelaniya.uni.inputs.JsonFileInputData;
+import lk.kelaniya.uni.repository.SqlDataRepository;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 public class DataReportFactoryTest {
+
+    @Test
+    public void should_return_DayWiseSalesReport_object_when_reportType_is_day_wise_sale_report(){
+        DataReportFactory dataReportFactory = new DataReportFactory();
+        DataReport dataReport = dataReportFactory.getInstance(
+                "day-wise-sale-report",
+               "2005-02-11",
+               "2005-03-11",
+                new SqlDataRepository(new JsonFileInputData())
+        );
+
+        assertThat(dataReport,instanceOf(DayWiseSalesReport.class));
+    }
+
 
 }
