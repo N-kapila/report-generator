cc3c9bd074eb5209ef410e3ee430495f26690e39
N-kapila <nirmalkapilarathne1998@gmail.com>
2021-10-03 13:47:36 +0530

    change caption

--- src/main/java/lk/kelaniya/uni/Main.java
+++ src/main/java/lk/kelaniya/uni/Main.java
@@ -3,7 +3,7 @@ package lk.kelaniya.uni;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to JAVA Report Generator");
+        System.out.println("Welcome to JAVA Report Generator!");
     }
 
 }
