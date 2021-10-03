60d513880487542a094b56bb5d51a7cc5161f0b6
N-kapila <nirmalkapilarathne1998@gmail.com>
2021-10-03 15:34:48 +0530

    integrate json inputs

--- src/main/java/lk/kelaniya/uni/Main.java
+++ src/main/java/lk/kelaniya/uni/Main.java
@@ -1,8 +1,23 @@
 package lk.kelaniya.uni;
 
+import lk.kelaniya.uni.inputs.*;
+import lk.kelaniya.uni.repository.DataRepository;
+import lk.kelaniya.uni.repository.SqlDataRepository;
+
 public class Main {
 
     public static void main(String[] args) {
+//        Inputs inputs = new CommandLineInputs(args);
+//        DataRepository dataRepository = new SqlDataRepository();
+        Inputs jsonFileInpust = new JsonFileInputs();
+        try {
+            JsonFileInputData data =(JsonFileInputData) jsonFileInpust.getInputs();
+            System.out.println(data.getDatabaseHost());
+        } catch (InputException e) {
+          System.out.println(e.getMessage());
+        }
+
+
         System.out.println("Welcome to JAVA Report Generator!");
     }
 
---
+++ src/main/java/lk/kelaniya/uni/ReportGeneratorApp.java
@@ -0,0 +1,4 @@
+package lk.kelaniya.uni;
+
+public class ReportGeneratorApp {
+}
